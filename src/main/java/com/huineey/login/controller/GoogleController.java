4df992fb6ac25bf1399cf769c02825b84645fa75
dahuin-1 <dawhite01@naver.com>
2021-10-29 03:59:36 +0900

    edit code

--- src/main/java/com/huineey/login/controller/GoogleController.java
+++ src/main/java/com/huineey/login/controller/GoogleController.java
@@ -18,6 +18,7 @@ import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 @Controller
@@ -39,37 +40,40 @@ public class GoogleController {
     @GetMapping("/google/callback")
     public String GetRequestToken(HttpServletRequest request) throws JsonParseException, JsonMappingException, IOException {
 
-        //RestTemplate을 사용하여 Access Token 및 profile을 요청한다.
+        //RestTemplate을 사용하여 Access Token 및 profile을 요청
         RestTemplate restTemplate = new RestTemplate();
         MultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
+
+        //파라미터
         parameters.add("code", request.getParameter("code"));
-        parameters.add("client_id", "1051168153056-m9v7nto44bat0kk2piga3d2ler24do5r.apps.googleusercontent.com");
-        parameters.add("client_secret", "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");
-        parameters.add("redirect_uri", googleoAuth2Parameters.getRedirectUri());
+        parameters.add("clientId", "1051168153056-m9v7nto44bat0kk2piga3d2ler24do5r.apps.googleusercontent.com");
+        parameters.add("clientSecret", "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");
+        parameters.add("redirectUri", googleoAuth2Parameters.getRedirectUri());
         parameters.add("grant_type", "authorization_code");
 
+        //headers 설정한다
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
         HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<MultiValueMap<String, String>>(parameters, headers);
         ResponseEntity<Map> responseEntity = restTemplate.exchange("https://www.googleapis.com/oauth2/v4/token", HttpMethod.POST, requestEntity, Map.class);
-        Map<String, Object> responseMap = responseEntity.getBody();
+        Map responseMap = responseEntity.getBody();
 
-        System.out.println(responseEntity.toString());
+        //System.out.println(responseEntity); //POST요청을 통해서 받은 응답출력
 
-
+        assert responseMap != null;
         String[] tokens = ((String)responseMap.get("id_token")).split("\\.");
         Base64 base64 = new Base64(true);
         String body = new String(base64.decode(tokens[1]));
 
         System.out.println(tokens.length);
-        System.out.println(new String(Base64.decodeBase64(tokens[0]), "utf-8"));
-        System.out.println(new String(Base64.decodeBase64(tokens[1]), "utf-8"));
+        System.out.println(new String(Base64.decodeBase64(tokens[0]), StandardCharsets.UTF_8)); //한글로 변환한다
+        System.out.println(new String(Base64.decodeBase64(tokens[1]), StandardCharsets.UTF_8));
+        //System.out.println(new String(Base64.decodeBase64(tokens[2]), StandardCharsets.UTF_8));
 
-        //Jackson을 사용한 JSON을 자바 Map 형식으로 변환
+        //JSON(by Jackson)을 Map 형식으로 변환
         ObjectMapper mapper = new ObjectMapper();
-        Map<String, String> result = mapper.readValue(body, Map.class);
+        Map result = mapper.readValue(body, Map.class);
         System.out.println(result.get(""));
-
 
         return "redirect:http://localhost:8080/index.html";
     }
